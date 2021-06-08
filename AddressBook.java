9cc47bcb9a2d1ecc972c1bcc7070dff13ab407d8
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-08 19:55:13 +0530

    Welcome message

--- AddressBook.java
+++ AddressBook.java
@@ -1,7 +1,13 @@
 package details;
+import model.Person;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.lang.Integer;
 public class AddressBook {
     public static void main(String[] args) {
         System.out.println("Welcome to address book");
+
     }
 }
 
